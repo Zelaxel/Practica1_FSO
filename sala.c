af0338b97399add8c905f8a42dbea5f0db756d08
Zelaxel <alejandroramirezlorenzo2003@gmail.com>
2025-03-04 19:03:10 +0000

    sala.c

--- sala.c
+++ sala.c
@@ -5,9 +5,30 @@ int* Sala_Teatro = NULL;
 int capacidad_total = 0;
 
 int reservar_asiento(int id_persona){
-	if(Sala_Teatro==NULL){
-		return -1;
+	if(Sala_Teatro==NULL) return -1;
+	for(int i=0; i<capacidad; i++){
+		if(Sala_Teatro[i] > -1){
+			Sala_Teatro[i] = id_persona;
+			return i;
+		}
 	}
+	return -1;
+}
+
+int libera_asiento(int id_asiento){
+	if(Sala_Teatro==NULL) return -1;
+	if(id_asiento < capacidad_total && 0 <= id_asiento){
+		int id_persona = Sala_Teatro[id_asiento];
+		Sala_Teatro[id_asiento]=-1;
+		return id_persona;
+	}
+	return -1;
+}
+
+int estado_asiento(int id_asiento){
+	if(Sala_Teatro==NULL) return -1;
+	if(Sala_Teatro[id_asiento] > -1) return 0;
+	return -1;
 }
 
 int asientos_ocupados(){
@@ -15,12 +36,17 @@ int asientos_ocupados(){
         return -1;
     }
     int ocupados = 0;
-    for(int i=0; i<=capacidad; i++){
+    for(int i=0; i<capacidad; i++){
         if(Sala_Teatro[i] == -1){
             ocupados++;
         }
     }
     return ocupados;
+}
+
+int asientos_libres(){
+	if(Sala_Teatro==NULL) return -1;
+	return capacidad - asientos_ocupados();
 }
 
 int capacidad_sala(){
@@ -36,7 +62,7 @@ int crear_sala(int capacidad){
     }
     capacidad_total = capacidad;
     Sala_Teatro = malloc(capacidad*sizeof(int));
-    for(int i=0; i<=capacidad; i++){
+    for(int i=0; i<capacidad; i++){
         Sala_Teatro[i] = -1;
     }
     return capacidad;
