1c824385c3ec4e9b1848721bd28f30a195583d55
TheRubils <rubengc2120@gmail.com>
2025-03-04 18:06:29 +0000

    Crear_sala
    La funcion "Crear_sala" fue implementada

---
+++ sala.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* Sala_Teatro;
+
+int crear_sala(int capacidad){
+    if(strlen(Sala_Teatro) != NULL){
+        return -1;
+    }
+    Sala_Teatro = malloc(capacidad*sizeof(int));
+    for(int i=0; i<=capacidad; i++){
+        Sala_Teatro[i] = -1;
+    }
+    return capacidad;
+}
